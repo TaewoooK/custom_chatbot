08a6462ba64b2b54a9868897cd0976e91e94b641
Tae Woo Kim <eddiekim1125@gmail.com>
2024-07-07 23:57:17 -0400

    make it smarter

--- src/app/taebot/NavBar.tsx
+++ src/app/taebot/NavBar.tsx
@@ -1,26 +1,24 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/app/assets/logo.png"
+import logo from "@/app/assets/logo.png";
 import { Button } from "@/components/ui/button";
 import ThemeToggleButton from "../components/ThemeToggleButton";
 
-
 export default function NavBar() {
-
-    return ( 
-        <div className="p-4 shadow">
-            <div className="flex flex-wrap gap-3 items-center justify-between">
-                <Link href="taebot" className="flex items-center gap-1">
-                    <Image src={logo} alt="TaeBot logo" width={40} height={40} />
-                    <span className="font-bold">TaeBot</span>
-                </Link>
-                <div className="flex items-center gap-2">
-                    <ThemeToggleButton />
-                    <Button asChild>
-                        <Link href="/taebot">Report</Link>
-                    </Button>
-                </div>
-            </div>
+  return (
+    <div className="p-4 shadow">
+      <div className="flex flex-wrap items-center justify-between gap-3">
+        <Link href="/" className="flex items-center gap-1">
+          <Image src={logo} alt="TaeBot logo" width={40} height={40} />
+          <span className="font-bold">TaeBot</span>
+        </Link>
+        <div className="flex items-center gap-2">
+          <ThemeToggleButton />
+          <Button asChild>
+            <Link href="/taebot">Report</Link>
+          </Button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }

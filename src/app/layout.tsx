1aaa5682e700359a40fce9330c5399676147e53c
Tae Woo Kim <eddiekim1125@gmail.com>
2024-07-08 00:57:41 -0400

    use analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./ThemeProvider";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}><ThemeProvider attribute="class">{children}</ThemeProvider></body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
+      </body>
+      <Analytics />
     </html>
   );
 }
--- src/app/taebot/layout.tsx
+++ src/app/taebot/layout.tsx
@@ -1,17 +1,16 @@
 import NavBar from "./NavBar";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
-export default function Layout({
-   children,
-}: {
-  children: React.ReactNode;  
-}) {
-    return <>
-
-    <div  className="h-dvh"> 
-    <div className="h-full"><NavBar />
-        <main className="p-4 max-w-7xl m-auto">{children}</main> </div> 
-          
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <div className="h-dvh">
+        <div className="h-full">
+          <NavBar />
+          <main className="m-auto max-w-7xl p-4">{children}</main>{" "}
         </div>
+        <Analytics />
+      </div>
     </>
+  );
 }
